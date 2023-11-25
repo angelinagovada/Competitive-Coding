0c216abfb621574a10d693267887aaf2468bca93
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2023-11-25 16:58:38 -0600

    Time: 49 ms (64.15%) | Memory: 17.2 MB (38.14%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        '''
+        s = s.replace(" ","")
+        s = s.replace(",","")
+        s = s.replace(":","")
+        s = s.replace(".","")
+        s = s.replace("@","")
+
+        '''
+
+        
+        s1=""
+        for i in s:
+            if(i.isalnum()):
+                s1 = s1+i.lower()
+        
+        s2 = s1[::-1]
+        print(s)
+        print(s1)
+        print(s2)
+
+        return(s1==s2)
+
+        
