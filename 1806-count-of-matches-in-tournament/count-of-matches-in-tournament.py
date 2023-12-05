08ac53305866a664dcb01dcb4ef41689b711436f
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2023-12-05 00:46:42 -0600

    Time: 39 ms (44.26%) | Memory: 16.4 MB (5.91%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        c = 0
+        while(n!=1):
+
+            if(n%2!=0):
+                n = n//2 
+                c = c+n 
+                n = n + 1
+                print(n)
+            else:
+                n = n//2 
+                c = c+n
+                print(n)
+                
+        
+        return c
