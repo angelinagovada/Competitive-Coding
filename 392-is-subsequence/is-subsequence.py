74d00279c94f618363b2e986a554f818112bba32
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2023-12-30 11:37:44 -0600

    Time: 75 ms (5.42%) | Memory: 17.7 MB (6.49%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        t = [ i for i in t]
+        ans =  "true"
+
+
+        for j in s:
+            print("j: ",j)
+            if (j not in t):
+                return False
+            else:
+                k = t.index(j)
+                t = t[k+1:]
+                print("t: ",t)
+
+        return True
+        
