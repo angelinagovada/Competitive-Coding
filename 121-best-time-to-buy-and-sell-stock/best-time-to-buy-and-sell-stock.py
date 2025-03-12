d907f7dbda4090fd1597d40c5557430d9846231c
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2025-03-11 19:44:07 -0500

    Time: 41 ms (80.37%) | Memory: 26.9 MB (77.56%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,30 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-       
-        '''  TIME LIMIT EXCEEDED - BRUTE FORCE 
-        profit=0
+        left = 0
+        right= 1
 
-        for buy in range(len(prices)):
-            sell=buy+1
-            while(sell<len(prices)):
-                if(prices[buy]<prices[sell] and p< prices[sell]-prices[buy]):
-                    p = prices[sell]-prices[buy]
-                sell=+1
-
-        return profit
-
-        '''
-
-        buy, sell = 0, 1
         profit = 0
 
-        while(sell<len(prices)):
-            if(prices[sell]>prices[buy]):
-                profit = max(profit,prices[sell]-prices[buy])
-                sell = sell + 1
-            else:
-                buy  = sell
-                sell = sell + 1
+        for right in range(1, len(prices)):
+            if (prices[right] < prices[left] ):
+               left = right
+                #right +=1 
+            elif( profit < prices[right] - prices[left] ):
+                profit = prices[right] - prices[left]
+                #right += 1
+
+
         return profit
 
 
