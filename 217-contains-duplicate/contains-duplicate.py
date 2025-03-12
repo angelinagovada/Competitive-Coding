fa22db00eacc9c8fdc95d7ae81df763ac8f016fb
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2025-03-11 20:04:42 -0500

    Time: 44 ms (8.37%) | Memory: 34.9 MB (18.13%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,3 +1,18 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return(len(nums)>len(set(nums)))
+    def containsDuplicate(self, nums: List[int]) -> bool: 
+        ans = False
+        HashMap = {}
+
+        for i in nums:
+            if i not in HashMap:
+                HashMap[i] = 1
+            else:
+                HashMap[i] +=1
+
+        if(max(HashMap.values())>=2):
+            print(max(HashMap))
+            ans = True
+        
+        return ans
+
+        
