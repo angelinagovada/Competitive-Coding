7c9a39f24c2b4622f4e62f02108c8130cc98ddf1
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2025-03-13 00:49:34 -0500

    Time: 33 ms (30.21%) | Memory: 27.1 MB (11.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,5 +1,8 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        ####### pre, pos, res ########
+
         N = len(nums)
         pre = [1]*N
         pos = [1]*N
@@ -15,6 +18,25 @@ class Solution:
             res[j] = pre[j]*pos[j]
 
         return res
+        
+
+        '''  To reduce space omplexity  --- FAILEDDD 
+        N = len(nums)
+        res = [1]*N
+
+        for i in range(1,N):
+            res[i]=res[i-1]*nums[i-1]
+
+        
+        right = 1
+        for k in range(N-2,-1,-1):
+            res[k]=res[k]*right
+            right = right*nums[i]
+
+        return res
+        '''
+
+
 
 
         
