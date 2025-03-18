5add7dbb7e5a4de2ddba823cb5363ef0ea8e945f
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2025-03-17 20:02:52 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (31.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -10,24 +10,24 @@ class Solution:
         #print(mid)
         #Binary(first,last,nums)
 
-        if(nums[right]>=nums[0]):
-            return nums[0]
+        if(nums[left]<nums[right]):
+            return nums[left]
+        else:
+            while(right>left):
+                if(nums[mid-1] > nums[mid]):
+                    return nums[mid]
+                elif(nums[mid] > nums[mid+1]):
+                    return nums[mid+1]
+                else:
+                    if(nums[mid]>nums[left]):
+                        left = mid+1
+                    else:
+                        right = mid-1
 
-        # nums[mid]>nums[mid+1] or nums[mid-1] <nums[mid]
-        while ( right>=left):
-            if (nums[mid-1]>nums[mid]):
-                return nums[mid]
-            if (nums[mid]>nums[mid+1]):
-                return nums[mid+1]
-            
-            if (nums[mid]>nums[left] and nums[mid]>nums[right]):
-                left = mid+1
-            else:
-                right = mid-1
-            print((left+right)//2)
-            mid=(left+right)//2
-            print(mid)
-            res=min(res,nums[mid])
+                    res = min (res,nums[mid])
+                    mid = (left+right)//2
 
-        return (res)
+        return res
+
+       
 
