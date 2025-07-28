cc1e5fe6613987fbc9c0d8bc104c9b98bf9c6e30
Kundana Angelina Govada <46242563+angelinagovada@users.noreply.github.com>
2025-07-28 16:11:20 -0500

    Time: 3 ms (79.00%) | Memory: 17.9 MB (40.05%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,25 +1,21 @@
-values = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-}
-
-
 class Solution:
     def romanToInt(self, s: str) -> int:
-        total = 0
-        i = 0
-        while i < len(s):
-            # If this is the subtractive case.
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
-                total += values[s[i + 1]] - values[s[i]]
-                i += 2
-            # Else this is NOT the subtractive case.
-            else:
-                total += values[s[i]]
-                i += 1
-        return total
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res=0
+
+        for i in range(len(s)-1):
+            if romans[s[i]]>romans[s[i+1]] or romans[s[i]]==romans[s[i+1]]:
+                res = res+romans[s[i]]
+            elif romans[s[i]]<romans[s[i+1]]:
+                res = res-romans[s[i]]
+
+        res=res+romans[s[-1]]   
+
+        return res
+
+
+
+
+
+
+        
